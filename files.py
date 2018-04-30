909280f9429baee9edea709801731f83b62ce677
renukamani <36950900+renukamani@users.noreply.github.com>
2018-04-30 12:00:53 +0530

    Add files via upload

---
+++ files.py
@@ -0,0 +1,39 @@
+import sys
+import threading
+
+class myThread(threading.Thread) :
+    def __init__(self,f_name,fh) :
+        threading.Thread.__init__(self)
+        self.f_name = f_name
+        self.fh = fh
+        self.line_count=0
+        self.word_count=0
+        self.char_count=0
+    def run(self) :
+        for line in self.fh :
+            self.line_count += 1
+            line = line.strip()
+            words = line.split()
+            self.word_count += len(words)
+            for word in words :
+                for letter in word :
+                    self.char_count += 1
+        print self.f_name,self.line_count,self.word_count,self.char_count
+
+lst = sys.argv
+lst.pop(0)
+fhl = list()
+
+#creating file handles
+for i in range(len(lst)) :
+    fhl.append(open(lst[i]))
+
+thread = list()
+
+#creating new threads
+for i in range(len(lst)) :
+    thread.append(myThread(lst[i],fhl[i]))
+
+#starting threads
+for i in range(len(lst)) :
+    thread[i].start()
